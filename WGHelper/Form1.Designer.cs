e7482aba36c7e19475bcb0bd38efb2eae239dcd1
M0stwan1eD <M0stwan1eD@M0STWAN1ED-PC>
2016-09-20 04:04:32 +0300

    Создано основную форму приложения, Создан запрос и выведение на экран статус серверов

---
+++ WGHelper/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace WGHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_GetWoTPlayersOnline = new System.Windows.Forms.Button();
+            this.label_totalOnline = new System.Windows.Forms.Label();
+            this.label_WoT_ru1 = new System.Windows.Forms.Label();
+            this.label_WoT_ru2 = new System.Windows.Forms.Label();
+            this.label_WoT_ru3 = new System.Windows.Forms.Label();
+            this.label_WoT_ru4 = new System.Windows.Forms.Label();
+            this.label_WoT_ru5 = new System.Windows.Forms.Label();
+            this.label_WoT_ru6 = new System.Windows.Forms.Label();
+            this.label_WoT_ru7 = new System.Windows.Forms.Label();
+            this.label_WoT_ru8 = new System.Windows.Forms.Label();
+            this.label_WoT_ru9 = new System.Windows.Forms.Label();
+            this.label_WoT_ru10 = new System.Windows.Forms.Label();
+            this.label_total = new System.Windows.Forms.Label();
+            this.label_ru1Online = new System.Windows.Forms.Label();
+            this.label_ru2Online = new System.Windows.Forms.Label();
+            this.label_ru3Online = new System.Windows.Forms.Label();
+            this.label_ru4Online = new System.Windows.Forms.Label();
+            this.label_ru5Online = new System.Windows.Forms.Label();
+            this.label_ru6Online = new System.Windows.Forms.Label();
+            this.label_ru7Online = new System.Windows.Forms.Label();
+            this.label_ru8Online = new System.Windows.Forms.Label();
+            this.label_ru9Online = new System.Windows.Forms.Label();
+            this.label_ru10Online = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage_WoT = new System.Windows.Forms.TabPage();
+            this.tabPage_WoWp = new System.Windows.Forms.TabPage();
+            this.tabPage_WoWs = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage_WoT.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Helper for Wargaming.net Games";
+            // 
+            // button_GetWoTPlayersOnline
+            // 
+            this.button_GetWoTPlayersOnline.Location = new System.Drawing.Point(20, 16);
+            this.button_GetWoTPlayersOnline.Name = "button_GetWoTPlayersOnline";
+            this.button_GetWoTPlayersOnline.Size = new System.Drawing.Size(309, 23);
+            this.button_GetWoTPlayersOnline.TabIndex = 1;
+            this.button_GetWoTPlayersOnline.Text = "button_GetWoTPlayersOnline";
+            this.button_GetWoTPlayersOnline.UseVisualStyleBackColor = true;
+            this.button_GetWoTPlayersOnline.Click += new System.EventHandler(this.button_GetWoTPlayersOnline_Click);
+            // 
+            // label_totalOnline
+            // 
+            this.label_totalOnline.AutoSize = true;
+            this.label_totalOnline.Location = new System.Drawing.Point(165, 224);
+            this.label_totalOnline.Name = "label_totalOnline";
+            this.label_totalOnline.Size = new System.Drawing.Size(85, 13);
+            this.label_totalOnline.TabIndex = 2;
+            this.label_totalOnline.Text = "label_totalOnline";
+            // 
+            // label_WoT_ru1
+            // 
+            this.label_WoT_ru1.AutoSize = true;
+            this.label_WoT_ru1.Location = new System.Drawing.Point(114, 62);
+            this.label_WoT_ru1.Name = "label_WoT_ru1";
+            this.label_WoT_ru1.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru1.TabIndex = 3;
+            this.label_WoT_ru1.Text = "RU1";
+            // 
+            // label_WoT_ru2
+            // 
+            this.label_WoT_ru2.AutoSize = true;
+            this.label_WoT_ru2.Location = new System.Drawing.Point(114, 75);
+            this.label_WoT_ru2.Name = "label_WoT_ru2";
+            this.label_WoT_ru2.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru2.TabIndex = 4;
+            this.label_WoT_ru2.Text = "RU2";
+            // 
+            // label_WoT_ru3
+            // 
+            this.label_WoT_ru3.AutoSize = true;
+            this.label_WoT_ru3.Location = new System.Drawing.Point(114, 88);
+            this.label_WoT_ru3.Name = "label_WoT_ru3";
+            this.label_WoT_ru3.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru3.TabIndex = 5;
+            this.label_WoT_ru3.Text = "RU3";
+            // 
+            // label_WoT_ru4
+            // 
+            this.label_WoT_ru4.AutoSize = true;
+            this.label_WoT_ru4.Location = new System.Drawing.Point(114, 101);
+            this.label_WoT_ru4.Name = "label_WoT_ru4";
+            this.label_WoT_ru4.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru4.TabIndex = 6;
+            this.label_WoT_ru4.Text = "RU4";
+            // 
+            // label_WoT_ru5
+            // 
+            this.label_WoT_ru5.AutoSize = true;
+            this.label_WoT_ru5.Location = new System.Drawing.Point(114, 114);
+            this.label_WoT_ru5.Name = "label_WoT_ru5";
+            this.label_WoT_ru5.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru5.TabIndex = 7;
+            this.label_WoT_ru5.Text = "RU5";
+            // 
+            // label_WoT_ru6
+            // 
+            this.label_WoT_ru6.AutoSize = true;
+            this.label_WoT_ru6.Location = new System.Drawing.Point(114, 127);
+            this.label_WoT_ru6.Name = "label_WoT_ru6";
+            this.label_WoT_ru6.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru6.TabIndex = 8;
+            this.label_WoT_ru6.Text = "RU6";
+            // 
+            // label_WoT_ru7
+            // 
+            this.label_WoT_ru7.AutoSize = true;
+            this.label_WoT_ru7.Location = new System.Drawing.Point(114, 140);
+            this.label_WoT_ru7.Name = "label_WoT_ru7";
+            this.label_WoT_ru7.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru7.TabIndex = 9;
+            this.label_WoT_ru7.Text = "RU7";
+            // 
+            // label_WoT_ru8
+            // 
+            this.label_WoT_ru8.AutoSize = true;
+            this.label_WoT_ru8.Location = new System.Drawing.Point(114, 153);
+            this.label_WoT_ru8.Name = "label_WoT_ru8";
+            this.label_WoT_ru8.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru8.TabIndex = 10;
+            this.label_WoT_ru8.Text = "RU8";
+            // 
+            // label_WoT_ru9
+            // 
+            this.label_WoT_ru9.AutoSize = true;
+            this.label_WoT_ru9.Location = new System.Drawing.Point(114, 166);
+            this.label_WoT_ru9.Name = "label_WoT_ru9";
+            this.label_WoT_ru9.Size = new System.Drawing.Size(29, 13);
+            this.label_WoT_ru9.TabIndex = 11;
+            this.label_WoT_ru9.Text = "RU9";
+            // 
+            // label_WoT_ru10
+            // 
+            this.label_WoT_ru10.AutoSize = true;
+            this.label_WoT_ru10.Location = new System.Drawing.Point(114, 179);
+            this.label_WoT_ru10.Name = "label_WoT_ru10";
+            this.label_WoT_ru10.Size = new System.Drawing.Size(35, 13);
+            this.label_WoT_ru10.TabIndex = 12;
+            this.label_WoT_ru10.Text = "RU10";
+            // 
+            // label_total
+            // 
+            this.label_total.AutoSize = true;
+            this.label_total.Location = new System.Drawing.Point(114, 224);
+            this.label_total.Name = "label_total";
+            this.label_total.Size = new System.Drawing.Size(31, 13);
+            this.label_total.TabIndex = 13;
+            this.label_total.Text = "Total";
+            // 
+            // label_ru1Online
+            // 
+            this.label_ru1Online.AutoSize = true;
+            this.label_ru1Online.Location = new System.Drawing.Point(165, 62);
+            this.label_ru1Online.Name = "label_ru1Online";
+            this.label_ru1Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru1Online.TabIndex = 14;
+            this.label_ru1Online.Text = "label_ru1Online";
+            // 
+            // label_ru2Online
+            // 
+            this.label_ru2Online.AutoSize = true;
+            this.label_ru2Online.Location = new System.Drawing.Point(165, 75);
+            this.label_ru2Online.Name = "label_ru2Online";
+            this.label_ru2Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru2Online.TabIndex = 15;
+            this.label_ru2Online.Text = "label_ru2Online";
+            // 
+            // label_ru3Online
+            // 
+            this.label_ru3Online.AutoSize = true;
+            this.label_ru3Online.Location = new System.Drawing.Point(165, 88);
+            this.label_ru3Online.Name = "label_ru3Online";
+            this.label_ru3Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru3Online.TabIndex = 16;
+            this.label_ru3Online.Text = "label_ru3Online";
+            // 
+            // label_ru4Online
+            // 
+            this.label_ru4Online.AutoSize = true;
+            this.label_ru4Online.Location = new System.Drawing.Point(165, 101);
+            this.label_ru4Online.Name = "label_ru4Online";
+            this.label_ru4Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru4Online.TabIndex = 17;
+            this.label_ru4Online.Text = "label_ru4Online";
+            // 
+            // label_ru5Online
+            // 
+            this.label_ru5Online.AutoSize = true;
+            this.label_ru5Online.Location = new System.Drawing.Point(165, 114);
+            this.label_ru5Online.Name = "label_ru5Online";
+            this.label_ru5Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru5Online.TabIndex = 18;
+            this.label_ru5Online.Text = "label_ru5Online";
+            // 
+            // label_ru6Online
+            // 
+            this.label_ru6Online.AutoSize = true;
+            this.label_ru6Online.Location = new System.Drawing.Point(165, 127);
+            this.label_ru6Online.Name = "label_ru6Online";
+            this.label_ru6Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru6Online.TabIndex = 19;
+            this.label_ru6Online.Text = "label_ru6Online";
+            // 
+            // label_ru7Online
+            // 
+            this.label_ru7Online.AutoSize = true;
+            this.label_ru7Online.Location = new System.Drawing.Point(165, 140);
+            this.label_ru7Online.Name = "label_ru7Online";
+            this.label_ru7Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru7Online.TabIndex = 20;
+            this.label_ru7Online.Text = "label_ru7Online";
+            // 
+            // label_ru8Online
+            // 
+            this.label_ru8Online.AutoSize = true;
+            this.label_ru8Online.Location = new System.Drawing.Point(165, 153);
+            this.label_ru8Online.Name = "label_ru8Online";
+            this.label_ru8Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru8Online.TabIndex = 21;
+            this.label_ru8Online.Text = "label_ru8Online";
+            // 
+            // label_ru9Online
+            // 
+            this.label_ru9Online.AutoSize = true;
+            this.label_ru9Online.Location = new System.Drawing.Point(165, 166);
+            this.label_ru9Online.Name = "label_ru9Online";
+            this.label_ru9Online.Size = new System.Drawing.Size(80, 13);
+            this.label_ru9Online.TabIndex = 22;
+            this.label_ru9Online.Text = "label_ru9Online";
+            // 
+            // label_ru10Online
+            // 
+            this.label_ru10Online.AutoSize = true;
+            this.label_ru10Online.Location = new System.Drawing.Point(165, 179);
+            this.label_ru10Online.Name = "label_ru10Online";
+            this.label_ru10Online.Size = new System.Drawing.Size(86, 13);
+            this.label_ru10Online.TabIndex = 23;
+            this.label_ru10Online.Text = "label_ru10Online";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage_WoT);
+            this.tabControl1.Controls.Add(this.tabPage_WoWp);
+            this.tabControl1.Controls.Add(this.tabPage_WoWs);
+            this.tabControl1.Location = new System.Drawing.Point(12, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(396, 299);
+            this.tabControl1.TabIndex = 24;
+            // 
+            // tabPage_WoT
+            // 
+            this.tabPage_WoT.Controls.Add(this.button_GetWoTPlayersOnline);
+            this.tabPage_WoT.Controls.Add(this.label_ru10Online);
+            this.tabPage_WoT.Controls.Add(this.label_totalOnline);
+            this.tabPage_WoT.Controls.Add(this.label_ru9Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru1);
+            this.tabPage_WoT.Controls.Add(this.label_ru8Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru2);
+            this.tabPage_WoT.Controls.Add(this.label_ru7Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru3);
+            this.tabPage_WoT.Controls.Add(this.label_ru6Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru4);
+            this.tabPage_WoT.Controls.Add(this.label_ru5Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru5);
+            this.tabPage_WoT.Controls.Add(this.label_ru4Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru6);
+            this.tabPage_WoT.Controls.Add(this.label_ru3Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru7);
+            this.tabPage_WoT.Controls.Add(this.label_ru2Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru8);
+            this.tabPage_WoT.Controls.Add(this.label_ru1Online);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru9);
+            this.tabPage_WoT.Controls.Add(this.label_total);
+            this.tabPage_WoT.Controls.Add(this.label_WoT_ru10);
+            this.tabPage_WoT.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_WoT.Name = "tabPage_WoT";
+            this.tabPage_WoT.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_WoT.Size = new System.Drawing.Size(388, 273);
+            this.tabPage_WoT.TabIndex = 0;
+            this.tabPage_WoT.Text = "World of Tanks";
+            this.tabPage_WoT.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_WoWp
+            // 
+            this.tabPage_WoWp.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_WoWp.Name = "tabPage_WoWp";
+            this.tabPage_WoWp.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_WoWp.Size = new System.Drawing.Size(388, 273);
+            this.tabPage_WoWp.TabIndex = 1;
+            this.tabPage_WoWp.Text = "World of Warplanes";
+            this.tabPage_WoWp.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_WoWs
+            // 
+            this.tabPage_WoWs.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_WoWs.Name = "tabPage_WoWs";
+            this.tabPage_WoWs.Size = new System.Drawing.Size(388, 273);
+            this.tabPage_WoWs.TabIndex = 2;
+            this.tabPage_WoWs.Text = "World of Warships";
+            this.tabPage_WoWs.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 350);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage_WoT.ResumeLayout(false);
+            this.tabPage_WoT.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_GetWoTPlayersOnline;
+        private System.Windows.Forms.Label label_totalOnline;
+        private System.Windows.Forms.Label label_WoT_ru1;
+        private System.Windows.Forms.Label label_WoT_ru2;
+        private System.Windows.Forms.Label label_WoT_ru3;
+        private System.Windows.Forms.Label label_WoT_ru4;
+        private System.Windows.Forms.Label label_WoT_ru5;
+        private System.Windows.Forms.Label label_WoT_ru6;
+        private System.Windows.Forms.Label label_WoT_ru7;
+        private System.Windows.Forms.Label label_WoT_ru8;
+        private System.Windows.Forms.Label label_WoT_ru9;
+        private System.Windows.Forms.Label label_WoT_ru10;
+        private System.Windows.Forms.Label label_total;
+        private System.Windows.Forms.Label label_ru2Online;
+        private System.Windows.Forms.Label label_ru3Online;
+        private System.Windows.Forms.Label label_ru4Online;
+        private System.Windows.Forms.Label label_ru5Online;
+        private System.Windows.Forms.Label label_ru6Online;
+        private System.Windows.Forms.Label label_ru7Online;
+        private System.Windows.Forms.Label label_ru8Online;
+        private System.Windows.Forms.Label label_ru9Online;
+        private System.Windows.Forms.Label label_ru10Online;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage_WoT;
+        private System.Windows.Forms.TabPage tabPage_WoWp;
+        private System.Windows.Forms.TabPage tabPage_WoWs;
+        private System.Windows.Forms.Label label_ru1Online;
+    }
+}
+
---
+++ WGHelper/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WGHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Wot
+        {
+            public int players_online { get; set; }
+            public string server { get; set; }
+        }
+
+        public class Data
+        {
+            public List<Wot> wot { get; set; }
+        }
+
+        public class wotOnlineRootObject
+        {
+            public string status { get; set; }
+            public Data data { get; set; }
+        }
+
+        public static string urlServerOnline = "https://api.worldoftanks.ru/wgn/servers/info/";
+        public static string urlServerOnlineRequest = "application_id=demo&game=wot";
+        public static string jsonAnswer;
+        public static wotOnlineRootObject wotOnline;
+
+        private void button_GetWoTPlayersOnline_Click(object sender, EventArgs e)
+        {
+            WebRequest requestServerOnline = WebRequest.Create(urlServerOnline + "?" + urlServerOnlineRequest);
+            WebResponse responseServerOnline =  requestServerOnline.GetResponse();
+            Stream answerStream = responseServerOnline.GetResponseStream();
+            StreamReader srAnswer = new StreamReader(answerStream);
+            jsonAnswer = srAnswer.ReadToEnd();
+            wotOnlineRootObject wotOnline = JsonConvert.DeserializeObject<wotOnlineRootObject>(jsonAnswer);
+
+            for (int i=0; i<wotOnline.data.wot.Count; i++)
+            {
+                switch(wotOnline.data.wot[i].server)
+                {
+                    case "RU1":
+                        {
+                            label_ru1Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU2":
+                        {
+                            label_ru2Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU3":
+                        {
+                            label_ru3Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU4":
+                        {
+                            label_ru4Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU5":
+                        {
+                            label_ru5Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU6":
+                        {
+                            label_ru6Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU7":
+                        {
+                            label_ru7Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU8":
+                        {
+                            label_ru8Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU9":
+                        {
+                            label_ru9Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                    case "RU10":
+                        {
+                            label_ru10Online.Text = wotOnline.data.wot[i].players_online.ToString();
+                            break;
+                        }
+                }
+            }
+
+            int wotTotalOnline = 0;
+            for (int i = 0; i < wotOnline.data.wot.Count; i++)
+            {
+                wotTotalOnline += Convert.ToInt32(wotOnline.data.wot[i].players_online);
+            }
+            label_totalOnline.Text = wotTotalOnline.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
