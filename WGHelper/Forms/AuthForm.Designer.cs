150473dae72eedb2b5a0998d59411414e9f742c4
M0stwan1eD <M0stwan1eD@M0STWAN1ED-PC>
2016-10-01 03:46:28 +0300

    Добавлена авторизация с помощью Wargaming OpenID

---
+++ WGHelper/Forms/AuthForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WGHelper.Forms
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Back = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Back
+            // 
+            this.button_Back.Location = new System.Drawing.Point(12, 350);
+            this.button_Back.Name = "button_Back";
+            this.button_Back.Size = new System.Drawing.Size(92, 23);
+            this.button_Back.TabIndex = 0;
+            this.button_Back.Text = "button_Back";
+            this.button_Back.UseVisualStyleBackColor = true;
+            this.button_Back.Click += new System.EventHandler(this.button_Back_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(931, 332);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://api.worldoftanks.ru/wot/auth/login/?application_id=146bc6b8d619f5030ed02c" +
+        "db5ce759b4&display=popup", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 352);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(833, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 383);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button_Back);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "AuthForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "AuthForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Back;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ WGHelper/Forms/AuthForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WGHelper.Forms
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        XDocument settings;
+
+        string access_token;
+        string nickname;
+        string account_id;
+        string experies_at;
+
+        private void button_Back_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            settings = XDocument.Load("settings.xml");
+            string url = webBrowser1.Url.ToString();
+            textBox1.Text = url;
+            if (url.Contains("status=ok") == true)
+            {
+                url = url.Replace("https://api.worldoftanks.ru/wot/blank/?&status=ok&", "");
+                for (int i = 0; i < url.Length; i++)
+                {
+                    if (url[i] == '&')
+                    {
+                        access_token = url.Substring(0, i+1);
+                        url = url.Replace(access_token, "");
+                        access_token = access_token.Replace("&", "");
+                        access_token = access_token.Replace("access_token=", "");
+                        settings.Element("settings").Element("wg_open_id").Element("access_token").Value = access_token;
+                        break;
+                    }
+                }
+                for (int i=0; i<url.Length; i++)
+                {
+                    if (url[i] == '&')
+                    {
+                        nickname = url.Substring(0, i+1);
+                        url = url.Replace(nickname, "");
+                        nickname = nickname.Replace("&", "");
+                        nickname = nickname.Replace("nickname=", "");
+                        settings.Element("settings").Element("wg_open_id").Element("nickname").Value = nickname;
+                        break;
+                    }
+                }
+                for (int i=0; i<url.Length; i++)
+                {
+                    if (url[i] == '&')
+                    {
+                        account_id = url.Substring(0, i+1);
+                        url = url.Replace(account_id, "");
+                        account_id = account_id.Replace("&", "");
+                        account_id = account_id.Replace("account_id=", "");
+                        experies_at = url.Replace("expires_at=", "");
+                        settings.Element("settings").Element("wg_open_id").Element("account_id").Value = account_id;
+                        settings.Element("settings").Element("wg_open_id").Element("experies_at").Value = experies_at;
+                        settings.Element("settings").Element("wg_open_id").Element("authorized").Value = "yes";
+                        break;
+                    }
+                }
+                this.Dispose();
+                settings.Save("settings.xml");
+            }
+        }
+    }
+}
